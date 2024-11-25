ab1851ec8d8a21026ab4300fc6f1161974afcabd
phil.depaoli@gmail.com <phil.depaoli@gmail.com>
2024-11-25 13:27:09 +0000

    kk

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,6 +1,6 @@
 
 -- Use the `ref` function to select from other models
 
-select *, 'TEST' as testcol
+select *, 'TEST' as testcol1, 'test2' as testcol2
 from {{ ref('my_first_dbt_model') }}
 where id = 1
